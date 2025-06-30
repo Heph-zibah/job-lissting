ae22bd4249d8ee61682444df4c33185fefca9516
oaDaramola <oluwatosinhephzibah@gmail.com>
2025-06-30 13:03:21 +0100

    added the background

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 
 import './index.css';
+import Home from "./views/Home";
 
 function App() {
 
   return (
     <>
-      <h1>hello</h1>
+      <Home />
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+
+
+const Header = () => {
+  return (
+    <header className="bg-[#61A8A8] bg-[url('/src/assets/images/bg-header-mobile.svg')] md:bg-[url('/src/assets/images/bg-header-desktop.svg')] bg-no-repeat bg-cover h-[20vh] flex items-center justify-center text-center">
+    </header>
+  );
+}
+
+export default Header
